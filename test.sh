88a47583bb9dd453a0a3521a5ba6e4ec9b87d306
EC2 Default User <ec2-user@ip-172-31-0-55.us-east-2.compute.internal>
2022-05-31 02:56:23 +0000

    Summary: Made scripts dynamic
    Now accepts user input as command line args and also checks if dir already exist

--- test.sh
+++ test.sh
@@ -1,13 +1,20 @@
 
 #/usr/bin/sh
-sudo useradd TestUser
-sudo su - TestUser
-mkdir Test
-cd Test/
-touch file1.txt
-touch file2.txt
-touch file3.txt
-mv file1.txt file1-2022-05-27.txt
-mv file2.txt file2-2022-05-27.txt
-mv file3.txt file3-2022-05-27.txt
-ls
+
+CONTENT_LIST=$1
+FILE_CT=$2
+DIR_NAME=$3
+
+if [ -d "$DIR_NAME" ]
+then
+	cd $DIR_NAME/
+else
+	mkdir $DIR_NAME
+	cd $DIR_NAME/
+fi
+
+for ((i=1; i<=$FILE_CT; i++))
+do
+	touch file_$i.txt
+	echo "${CONTENT_LIST}" > file_$i.txt
+done
